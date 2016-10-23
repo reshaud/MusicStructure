5c5c7529eaf3542f5aa767b766c9f471fb3fb04d
reshaud <reshaud.ally@gmail.com>
2016-10-23 17:38:35 -0400

    Added Activity Song and Artist

--- app/src/main/java/com/example/android/musicstructure/MainActivity.java
+++ app/src/main/java/com/example/android/musicstructure/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.android.musicstructure;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -19,6 +19,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(),Albums.class);
+                startActivity(i);
+            }
+        });
+
+
+        //Go to Artist activity
+        TextView artist = (TextView) findViewById(R.id.artists);
+        artist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), Artists.class);
                 startActivity(i);
             }
         });
---
+++ app/src/main/java/com/example/android/musicstructure/Songs.java
@@ -0,0 +1,13 @@
+package com.example.android.musicstructure;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Songs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song);
+    }
+}
